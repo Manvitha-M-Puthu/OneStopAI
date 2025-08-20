1495ca0330390e7f3426fb04fdbb09708be327e4
Manvitha M Puthu <manvithaputhu123@gmail.com>
2025-08-20 20:01:48 +0530

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -59,11 +59,6 @@ const Hero = () => {
                 </div>
                 
                 <div className='flex items-center gap-2 text-yellow-500'>
-                    {[...Array(5)].map((_, i) => (
-                        <svg key={i} className='w-4 h-4 fill-current' viewBox='0 0 20 20'>
-                            <path d='M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z'/>
-                        </svg>
-                    ))}
                     <span className='text-gray-600 text-sm ml-1'>4.9/5 rating</span>
                 </div>
             </div>
